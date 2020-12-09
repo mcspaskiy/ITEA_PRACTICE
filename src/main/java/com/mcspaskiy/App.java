32e2dedfb728a27b7ed5c285bb89fbacf1a8a099
Mikhail Spaskiy <mcspaskiy@gmail.com>
2020-12-09 16:27:45 +0200

    Added maven project

---
+++ src/main/java/com/mcspaskiy/App.java
@@ -0,0 +1,21 @@
+package com.mcspaskiy;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App extends Application
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
+    }
+}
