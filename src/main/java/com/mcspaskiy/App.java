16e5f9540a4ec8a019bb6fe52e7be3aa8b768fc5
Mikhail Spaskiy <mcspaskiy@gmail.com>
2020-12-09 17:16:51 +0200

    Added login form

--- src/main/java/com/mcspaskiy/App.java
+++ src/main/java/com/mcspaskiy/App.java
@@ -1,21 +1,34 @@
 package com.mcspaskiy;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 /**
- * Hello world!
- *
+ * создать в ФХ интерфейс формы авторизации с подключением к базе и собрать мейвеном,
+ * чтобы джарник заппускался под дабл клику
  */
-public class App extends Application
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App extends Application {
+    public static void main(String[] args) {
+        launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        
+    public void start(Stage primaryStage) {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("/login_layout.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("stylesheet.css");
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
     }
 }
