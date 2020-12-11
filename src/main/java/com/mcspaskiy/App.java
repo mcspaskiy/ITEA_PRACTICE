a8dc4db3eb71727f0757510d512e8c92a8bc0516
Mikhail Spaskiy <mcspaskiy@gmail.com>
2020-12-11 14:00:58 +0200

    Setup libgdx framework

--- src/main/java/com/mcspaskiy/App.java
+++ src/main/java/com/mcspaskiy/App.java
@@ -1,16 +1,15 @@
 package com.mcspaskiy;
 
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        new LwjglApplication(new MyGdxGame(), config);
-
+        new LwjglApplication(new TablutGame(), config);
     }
 }
