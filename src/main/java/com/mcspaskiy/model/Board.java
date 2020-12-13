177d82ae02743b5058ae19fb74e67164332e50e9
Mikhail Spaskiy <mcspaskiy@gmail.com>
2020-12-13 20:29:27 +0200

    Bug fixes

--- src/main/java/com/mcspaskiy/model/Board.java
+++ src/main/java/com/mcspaskiy/model/Board.java
@@ -246,77 +246,7 @@ public class Board {
          * Get piece on y-1. If it enemy get piece at x-2. If it friend or static - capture
          * Get piece on y+1. If it enemy get piece at x-2. If it friend or static - capture
          */
-        if (itemsOnBoard[x - 1][y] != null && itemsOnBoard[x - 1][y].getItemType() != pieceType) {
-            //The piece is present and it's an enemy
-            if (itemsOnBoard[x - 2][y] != null && itemsOnBoard[x - 2][y].getItemType() == pieceType) {
-                //The piece is present and it's a friend
-                ActiveItem capturedPiece = itemsOnBoard[x - 1][y];
-                itemsOnBoard[x - 1][y] = null;
-                if (capturedPiece.getItemType() == ItemType.BLACK) {
-                    capturedBlackPieces.add(capturedPiece);
-                    capturedPiece.spritePos(11, capturedBlackPieces.size());
-                    capturedPiece.setAlive(false);
-                } else if (capturedPiece.getItemType() == ItemType.WHITE) {
-                    capturedWhitePieces.add(capturedPiece);
-                    capturedPiece.spritePos(-3, capturedWhitePieces.size());
-                    capturedPiece.setAlive(false);
-                }
-            }
-        }
-
-        if (itemsOnBoard[x + 1][y] != null && itemsOnBoard[x + 1][y].getItemType() != pieceType) {
-            //The piece is present and it's an enemy
-            if (itemsOnBoard[x + 2][y] != null && itemsOnBoard[x + 2][y].getItemType() == pieceType) {
-                //The piece is present and it's a friend
-                ActiveItem capturedPiece = itemsOnBoard[x + 1][y];
-                itemsOnBoard[x + 1][y] = null;
-                if (capturedPiece.getItemType() == ItemType.BLACK) {
-                    capturedBlackPieces.add(capturedPiece);
-                    capturedPiece.spritePos(11, capturedBlackPieces.size());
-                    capturedPiece.setAlive(false);
-                } else if (capturedPiece.getItemType() == ItemType.WHITE) {
-                    capturedWhitePieces.add(capturedPiece);
-                    capturedPiece.spritePos(-3, capturedWhitePieces.size());
-                    capturedPiece.setAlive(false);
-                }
-            }
-        }
-
-        if (itemsOnBoard[x][y - 1] != null && itemsOnBoard[x][y - 1].getItemType() != pieceType) {
-            //The piece is present and it's an enemy
-            if (itemsOnBoard[x][y - 2] != null && itemsOnBoard[x][y - 2].getItemType() == pieceType) {
-                //The piece is present and it's a friend
-                ActiveItem capturedPiece = itemsOnBoard[x][y - 1];
-                itemsOnBoard[x][y - 1] = null;
-                if (capturedPiece.getItemType() == ItemType.BLACK) {
-                    capturedBlackPieces.add(capturedPiece);
-                    capturedPiece.spritePos(11, capturedBlackPieces.size());
-                    capturedPiece.setAlive(false);
-                } else if (capturedPiece.getItemType() == ItemType.WHITE) {
-                    capturedWhitePieces.add(capturedPiece);
-                    capturedPiece.spritePos(-3, capturedWhitePieces.size());
-                    capturedPiece.setAlive(false);
-                }
-            }
-        }
-
-        if (itemsOnBoard[x][y + 1] != null && itemsOnBoard[x][y + 1].getItemType() != pieceType) {
-            //The piece is present and it's an enemy
-            if (itemsOnBoard[x][y + 2] != null && itemsOnBoard[x][y + 2].getItemType() == pieceType) {
-                //The piece is present and it's a friend
-                ActiveItem capturedPiece = itemsOnBoard[x][y + 1];
-                itemsOnBoard[x][y + 1] = null;
-                if (capturedPiece.getItemType() == ItemType.BLACK) {
-                    capturedBlackPieces.add(capturedPiece);
-                    capturedPiece.spritePos(11, capturedBlackPieces.size());
-                    capturedPiece.setAlive(false);
-                } else if (capturedPiece.getItemType() == ItemType.WHITE) {
-                    capturedWhitePieces.add(capturedPiece);
-                    capturedPiece.spritePos(-3, capturedWhitePieces.size());
-                    capturedPiece.setAlive(false);
-                }
-            }
-        }
+        ruleProcessor.checkMovementResult(pieceType, x, y, itemsOnBoard, capturedWhitePieces, capturedBlackPieces);
     }
 
     private void clearAvailPositions() {
--- src/main/java/com/mcspaskiy/model/RulesProcessor.java
+++ src/main/java/com/mcspaskiy/model/RulesProcessor.java
@@ -1,4 +1,82 @@
 package com.mcspaskiy.model;
 
+import java.util.List;
+
 public class RulesProcessor {
+    public void checkMovementResult(ItemType pieceType, int x, int y, ActiveItem[][] itemsOnBoard,
+                                    List<ActiveItem> capturedWhitePieces, List<ActiveItem> capturedBlackPieces) {
+        //TODO: king can't capture the pieces
+
+        if (x >= 2 && itemsOnBoard[x - 1][y] != null && itemsOnBoard[x - 1][y].getItemType() != pieceType) {
+            //The piece is present and it's an enemy
+            if (itemsOnBoard[x - 2][y] != null && itemsOnBoard[x - 2][y].getItemType() == pieceType) {
+                //The piece is present and it's a friend
+                ActiveItem capturedPiece = itemsOnBoard[x - 1][y];
+                itemsOnBoard[x - 1][y] = null;
+                if (capturedPiece.getItemType() == ItemType.BLACK) {
+                    capturedBlackPieces.add(capturedPiece);
+                    capturedPiece.spritePos(11, capturedBlackPieces.size());
+                    capturedPiece.setAlive(false);
+                } else if (capturedPiece.getItemType() == ItemType.WHITE) {
+                    capturedWhitePieces.add(capturedPiece);
+                    capturedPiece.spritePos(-3, capturedWhitePieces.size());
+                    capturedPiece.setAlive(false);
+                }
+            }
+        }
+
+        if (x < itemsOnBoard.length - 2 && itemsOnBoard[x + 1][y] != null && itemsOnBoard[x + 1][y].getItemType() != pieceType) {
+            //The piece is present and it's an enemy
+            if (itemsOnBoard[x + 2][y] != null && itemsOnBoard[x + 2][y].getItemType() == pieceType) {
+                //The piece is present and it's a friend
+                ActiveItem capturedPiece = itemsOnBoard[x + 1][y];
+                itemsOnBoard[x + 1][y] = null;
+                if (capturedPiece.getItemType() == ItemType.BLACK) {
+                    capturedBlackPieces.add(capturedPiece);
+                    capturedPiece.spritePos(11, capturedBlackPieces.size());
+                    capturedPiece.setAlive(false);
+                } else if (capturedPiece.getItemType() == ItemType.WHITE) {
+                    capturedWhitePieces.add(capturedPiece);
+                    capturedPiece.spritePos(-3, capturedWhitePieces.size());
+                    capturedPiece.setAlive(false);
+                }
+            }
+        }
+
+        if (y >= 2 && itemsOnBoard[x][y - 1] != null && itemsOnBoard[x][y - 1].getItemType() != pieceType) {
+            //The piece is present and it's an enemy
+            if (itemsOnBoard[x][y - 2] != null && itemsOnBoard[x][y - 2].getItemType() == pieceType) {
+                //The piece is present and it's a friend
+                ActiveItem capturedPiece = itemsOnBoard[x][y - 1];
+                itemsOnBoard[x][y - 1] = null;
+                if (capturedPiece.getItemType() == ItemType.BLACK) {
+                    capturedBlackPieces.add(capturedPiece);
+                    capturedPiece.spritePos(11, capturedBlackPieces.size());
+                    capturedPiece.setAlive(false);
+                } else if (capturedPiece.getItemType() == ItemType.WHITE) {
+                    capturedWhitePieces.add(capturedPiece);
+                    capturedPiece.spritePos(-3, capturedWhitePieces.size());
+                    capturedPiece.setAlive(false);
+                }
+            }
+        }
+
+        if (y < itemsOnBoard.length -2 && itemsOnBoard[x][y + 1] != null && itemsOnBoard[x][y + 1].getItemType() != pieceType) {
+            //The piece is present and it's an enemy
+            if (itemsOnBoard[x][y + 2] != null && itemsOnBoard[x][y + 2].getItemType() == pieceType) {
+                //The piece is present and it's a friend
+                ActiveItem capturedPiece = itemsOnBoard[x][y + 1];
+                itemsOnBoard[x][y + 1] = null;
+                if (capturedPiece.getItemType() == ItemType.BLACK) {
+                    capturedBlackPieces.add(capturedPiece);
+                    capturedPiece.spritePos(11, capturedBlackPieces.size());
+                    capturedPiece.setAlive(false);
+                } else if (capturedPiece.getItemType() == ItemType.WHITE) {
+                    capturedWhitePieces.add(capturedPiece);
+                    capturedPiece.spritePos(-3, capturedWhitePieces.size());
+                    capturedPiece.setAlive(false);
+                }
+            }
+        }
+    }
 }
